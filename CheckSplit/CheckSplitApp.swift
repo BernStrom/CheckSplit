fce153e73a646846a5bd0a7015d4ae23fae6805f
Bern <bernnystrom@gmail.com>
2022-02-12 16:28:15 +0800

    Initial Commit

---
+++ CheckSplit/CheckSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitApp.swift
+//  CheckSplit
+//
+//  Created by Bern N on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
