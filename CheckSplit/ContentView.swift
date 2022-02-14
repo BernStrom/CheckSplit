fa66d6c69f8c44c687bc1b478eab9ed3376fdd22
Bern <bernnystrom@gmail.com>
2022-02-14 13:32:43 +0800

    🎉 (new): Set up app with basic layout and core funtionalities
    Started app with a basic Form layout and State properties for keeping track user data input.

--- CheckSplit/ContentView.swift
+++ CheckSplit/ContentView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .keyboardType(.decimalPad)
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2..<100) {
+                        Text("\($0) people")
+                    }
+                }
+            }
+            
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
