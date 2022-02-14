d034adb4a719b77a673c81ee8ab58123edcdd8c4
Bern <bernnystrom@gmail.com>
2022-02-14 14:12:10 +0800

    ✨ (new): Add a new section for tip percentage options
    Added a segmented Picker view for users to be able to select a range of tip percentages.

--- CheckSplit/ContentView.swift
+++ CheckSplit/ContentView.swift
@@ -29,6 +29,17 @@ struct ContentView: View {
                 }
                 
                 Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
                     Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
             }
