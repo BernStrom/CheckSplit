91afecef4a5bfab3edc2192231fc4bb11b8aa7c3
Bern <bernnystrom@gmail.com>
2022-02-16 13:37:53 +0800

    ✨ (new): Add larger range of choices for tip percentages
    Updated tip percentages to include a wider range of numbers for users to select from 0% to 100%.

--- CheckSplit/ContentView.swift
+++ CheckSplit/ContentView.swift
@@ -13,8 +13,6 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
     
-    let tipPercentages = [10, 15, 20, 25, 0]
-    
     var totalCheck: Double {
         let tipSelection = Double(tipPercentage)
         
@@ -52,11 +50,10 @@ struct ContentView: View {
                 
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0..<101, id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
